f1eb47c20ec494cae24425c81a9086657f9a360d
DarkMatter-999 <darkmatter999official@gmail.com>
2022-08-14 10:51:01 +0530

    Better output printing implementation

--- utils.js
+++ utils.js
@@ -6,27 +6,48 @@ export function getkey() {
 }
 
 export function printlog(c) {
-    var output = document.getElementById("output").innerText;
-    // console.log(c);
+    if(c === 10) {
+        println();
+    }
+    // var output = document.getElementById("output").innerText;
+    var output = "";
+    // // console.log(c);
     if (c === 32) {
         output += '\u00A0';     // shitty js
     } else {
         output += String.fromCharCode(c);
     }
 
-    document.getElementById("output").innerText = output;
+    globalThis.outputbuffer += output;
+
+    // document.getElementById("output").innerText = output;
     // console.log(c + "  " + String.fromCharCode(c));
     // console.log(String.fromCharCode(c));
 }
 
+export function println() {
+    document.getElementById("output").innerText += globalThis.outputbuffer;
+
+    globalThis.outputbuffer = "";
+}
+
+export function printdebug() {
+    document.getElementById("disassembly").innerText = globalThis.debugbuffer + document.getElementById("disassembly").innerText;
+    globalThis.debugbuffer = "";
+}
+
 export function printdisassembly(c) {
-    var output = document.getElementById("disassembly").innerText;
-    // console.log(c);
+    if(c === 10) {
+        printdebug();
+    }
+
+    var output = "";
+    // // console.log(c);
     if (c === 32) {
         output += '\u00A0';     // shitty js
     } else {
         output += String.fromCharCode(c);
     }
 
-    document.getElementById("disassembly").innerText = output;
+    globalThis.debugbuffer += output;
 }
